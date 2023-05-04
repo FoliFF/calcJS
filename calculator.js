dcff78c7dcf647329789ed478486a83f79e0306f
FoliFF <olof@hisham.se>
2023-05-04 23:35:31 +0200

    1st edition of my Calculator assignment

--- calculator.js
+++ calculator.js
@@ -12,7 +12,7 @@ function multiply(a, b) {
 
 function divide(a, b) {
   if (b === 0) {
-    return "Error: division by zero";
+    return "Does not compute";
   }
   return a / b;
 }
@@ -71,21 +71,81 @@ function handleEqualsClick() {
     return;
   }
   const currentValue = parseFloat(displayValue);
-  const result = operate(selectedOperator, storedValue, currentValue);
-  if (result !== null && result !== undefined) {
+  let result;
+  if (storedValue !== null) {
+    result = operate(selectedOperator, storedValue, currentValue);
     displayValue = result.toString();
-    storedValue = null;
-    selectedOperator = null;
-    updateDisplay();
+  } else {
+    result = currentValue;
   }
+  storedValue = result;
+  selectedOperator = null;
+  updateDisplay();
 }
 
 function handleDecimalClick(event) {
   const decimal = event.target.value;
+  // Only add a decimal point if there isn't one already
   if (!displayValue.includes(decimal)) {
     displayValue += decimal;
+    // Disable the decimal button if there's already a decimal point
+    const decimalButton = document.querySelector('.number[value="."]');
+    if (displayValue.includes(decimal)) {
+      decimalButton.disabled = true;
+    }
   }
   updateDisplay();
+}
+
+function handleBackspaceClick() {
+  displayValue = displayValue.slice(0, -1);
+  if (displayValue === "") {
+    displayValue = "0";
+  }
+  updateDisplay();
+}
+
+function handleKeyDown(event) {
+  const key = event.key;
+  switch (key) {
+    case "0":
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+      const numberButton = document.querySelector(`.number[value="${key}"]`);
+      numberButton.click();
+      break;
+    case "+":
+    case "-":
+    case "*":
+    case "/":
+      const operatorButton = document.querySelector(`.operator[value="${key}"]`);
+      operatorButton.click();
+      break;
+    case ".":
+      const decimalButton = document.querySelector('.number[value="."]');
+      decimalButton.click();
+      break;
+    case "Backspace":
+      const backButton = document.querySelector(".backspace");
+      backButton.click();
+      break;
+    case "Enter":
+      const equalsButton = document.querySelector(".equals");
+      equalsButton.click();
+      break;
+    case "Escape":
+      clearDisplay();
+      break;
+    default:
+      break;
+  }
 }
 
 const numberButtons = document.querySelectorAll(".number");
@@ -106,3 +166,6 @@ decimalButton.addEventListener("click", handleDecimalClick);
 
 const clearButton = document.querySelector(".clear");
 clearButton.addEventListener("click", clearDisplay);
+
+const backButton = document.querySelector(".backspace");
+backButton.addEventListener("click", handleBackspaceClick);
